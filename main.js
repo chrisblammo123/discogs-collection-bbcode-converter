5bea99193628e51daa9976d3203eed283779e867
Chris Struck <chrisblammo123@gmail.com>
2020-02-25 15:52:08 -0600

    Misc Updates
    Added comments and changed the format of the output for the script. Updated the package.json file.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const csv = require('fast-csv');
-const fs = require('fs');
+const csv = require('fast-csv');		//Used to parse the CSV file
+const fs = require('fs');						//Used to read the CSV file
 
 const collection = fs.createReadStream('collection.csv');
+//Sets the parameters for the parser
 const parser = csv.parse({
-	objectMode: true,
-	delimiter: ',',
-	headers: true
+	objectMode: true,			//Parses as an object
+	delimiter: ',',				//Tells the parser that the CSV uses the comma to differentiate between different entries
+	headers: true					//Tells the parser that the CSV uses headers
 });
 
 collection
@@ -13,7 +14,9 @@ collection
 	.on('error', error => console.error(error))
 	.on('readable', () => {
 		for (let release = parser.read(); release; release = parser.read()) {
-			console.log(`[URL='https://www.discogs.com/release/${release.release_id}']${release.Artist} - ${release.Title}[/URL] [Media: ${release['Collection Media Condition']} | Sleeve: ${release['Collection Sleeve Condition']}]`);
+			//My format to be output, can be easily changed to fit any need
+			console.log(`[URL='https://www.discogs.com/release/${release.release_id}']${release.Artist} - ${release.Title}[/URL] [ Media: ${release['Collection Media Condition']} | Sleeve: ${release['Collection Sleeve Condition']} ]`);
 		}
 	})
+	//Lists how many entries were parsed
 	.on('end', (rowCount) => console.log(`\nParsed ${rowCount} rows`));
